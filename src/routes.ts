0d1dedba22583d06276a1141856818b787e6c319
HMpina <henrique.marques.pina@gmail.com>
2023-06-09 00:04:07 -0300

    deploy no heroku5

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
-import { Router} from 'express'
+import { Router, Request, Response} from 'express'
 import { getTimesheets } from './controller/TimesheetsController';
 import { saveTimesheet } from './controller/TimesheetsController';
 import { getTimesheet } from './controller/TimesheetsController';
 import { updateTimesheet } from './controller/TimesheetsController';
 import { deleteTimesheet } from './controller/TimesheetsController';
 const routes = Router()
+ 
+routes.get('/home', (request: Request, response: Response) => {
+    return response.json({ message: 'Hello Turma 007!' })
+})
 routes.get('/timesheets', getTimesheets)
 routes.post('/timesheets', saveTimesheet)
 routes.get('/timesheets/:id', getTimesheet)
