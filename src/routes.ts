2cd58bdb7c8149ae3ace612e00876097c8f57c13
HMpina <henrique.marques.pina@gmail.com>
2023-06-08 23:12:59 -0300

    versão local final

---
+++ src/routes.ts
@@ -0,0 +1,13 @@
+import { Router, request, response, Request, Response} from 'express'
+import { getTimesheets } from './controller/TimesheetsController';
+import { saveTimesheet } from './controller/TimesheetsController';
+import { getTimesheet } from './controller/TimesheetsController';
+import { updateTimesheet } from './controller/TimesheetsController';
+import { deleteTimesheet } from './controller/TimesheetsController';
+const routes = Router()
+routes.get('/timesheets', getTimesheets)
+routes.post('/timesheets', saveTimesheet)
+routes.get('/timesheets/:id', getTimesheet)
+routes.put('/timesheets/:id', updateTimesheet)
+routes.delete('/timesheets/:id', deleteTimesheet)
+export default routes
